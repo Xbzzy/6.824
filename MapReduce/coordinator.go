23f7bc7171f1c78a1d374833082911caace17426
Xbzzy <57384383+Xbzzy@users.noreply.github.com>
2021-11-28 11:09:00 +0800

    Update coordinator.go

--- MapReduce/coordinator.go
+++ MapReduce/coordinator.go
@@ -1,32 +1,39 @@
-package MapReduce
+package mr
 
 //Coordinator is the master in paper MapReduce.
 import (
+	"errors"
 	"log"
 	"math/rand"
 	"net"
-	"net/rpc"
 	"net/http"
+	"net/rpc"
 )
+
 //string state including "idle"/"in-progress"/"completed"
 type state string
 //intermediate file args
 type fileName string
-type fileSize uint
+type fileSize int64
 type Coordinator struct {
-	NowID int
+	NowMapID int
 	// the int key of map is taskID,use to named intermediate files,mr-X-Y
 	MapTask map[int]fileName
 	MapTaskState map[int]state
+	UncompletedMapTaskNum int
 	ReduceTask map[int]fileName
 	ReduceTaskState map[int]state
+	UncompletedReduceTaskNum int
 	//updates to this location and size information when map tasks are completed.
 	IntermediateFiles map[string]fileSize
 }
 
 // Respond :an RPC handle function
 func (c *Coordinator) ApplyMapTask(args *Args, reply *Reply) error {
-	nowID := c.NowID
+	nowID := c.NowMapID
+	if args.ApplyMap == false {
+		return errors.New("apply fail")
+	}
 	for taskID,filename := range c.MapTask {
 		// choice an as-yet-unStarted map task
 		if taskID < 0 {
@@ -34,12 +41,45 @@ func (c *Coordinator) ApplyMapTask(args *Args, reply *Reply) error {
 			c.MapTask[nowID] = filename
 			c.MapTaskState[nowID] = "in-progress"
 			reply.MapFileName = string(c.MapTask[nowID])
-			c.NowID++
+			c.NowMapID++
 			return nil
 		} else {
 			continue
 		}
 	}
+	return nil
+}
+
+func (c *Coordinator) ApplyReduceTask(args *Args, reply *Reply) error {
+	if c.UncompletedReduceTaskNum == 0 {
+		reply.ReduceTaskID = -1
+		return nil
+	}
+	for {
+		reduceTaskID := ihash(args.Key) % 10
+		if c.ReduceTaskState[reduceTaskID] != "completed" {
+			reply.ReduceTaskID = reduceTaskID
+			reply.IntermediateFileName = string(c.ReduceTask[reduceTaskID])
+			return nil
+		} else {
+			continue
+		}
+	}
+}
+
+func (c *Coordinator) CorrectMapTaskState(args *Args,reply *Reply) error {
+	c.MapTaskState[args.MapCompletedID] = "completed"
+	c.IntermediateFiles[args.IntermediateFileName] = fileSize(args.FileSize)
+	c.UncompletedMapTaskNum--
+	c.UncompletedReduceTaskNum++
+	reply.RPCState = true
+	return nil
+}
+
+func (c *Coordinator) CorrectReduceTaskState(args *Args, reply *Reply) error {
+	c.ReduceTaskState[args.ReduceCompletedID] = "completed"
+	c.UncompletedReduceTaskNum--
+	reply.RPCState = true
 	return nil
 }
 
@@ -68,6 +108,13 @@ func (c *Coordinator) Done() bool {
 			return false
 		}
 	}
+	for _,stateResult:= range c.ReduceTaskState {
+		if stateResult == "completed"{
+			continue
+		} else {
+			return false
+		}
+	}
 	return true
 }
 
@@ -77,11 +124,14 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	coordinator := new(Coordinator)
+	mapTaskNum := 0
 	// Init map task
 	for _,str := range files {
 		random := -rand.Int()
 		coordinator.MapTask[random] = fileName(str)
+		mapTaskNum ++
 	}
+	coordinator.UncompletedMapTaskNum = mapTaskNum
 	// use max nReduce to init reduce task 'map'
 	for i:=0; i<nReduce; i++ {
 		coordinator.ReduceTask[i]=""
