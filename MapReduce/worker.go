b49f4f12a6bb8c9d8c364b7c173594371befc811
Xbzzy <57384383+Xbzzy@users.noreply.github.com>
2021-11-28 11:09:57 +0800

    Update worker.go

--- MapReduce/worker.go
+++ MapReduce/worker.go
@@ -1,14 +1,17 @@
 package mr
 
 import (
+	"encoding/json"
 	"fmt"
+	"hash/fnv"
 	"io/ioutil"
+	"log"
+	"net/rpc"
 	"os"
 	"sort"
+	"strconv"
 )
-import "log"
-import "net/rpc"
-import "hash/fnv"
+
 
 // Map functions return a slice of KeyValue.
 
@@ -36,36 +39,95 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
+// Worker :
 // main/mrworker.go calls this function.
 //
 func Worker(mapFunction func(string, string) []KeyValue,
 	reduceFunction func(string, []string) string) {
-	reply := Call()
-	filename := reply.MapFileName
-	intermediate := []KeyValue{}
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatalf("cannot open %v", filename)
-	}
-	content, err := ioutil.ReadAll(file)
-	if err != nil {
-		log.Fatalf("cannot read %v", filename)
-	}
-	file.Close()
-	kva := mapFunction(filename, string(content))
-	intermediate = append(intermediate, kva...)
-	sort.Sort(ByKey(intermediate))
-
+	go func() {
+		for {
+			args := new(Args)
+			args.ApplyMap = true
+			replyMapTask := RPCCall(args)
+			if replyMapTask.MapTaskID == -1 {
+				break
+			}
+			filename := replyMapTask.MapFileName
+			intermediate := []KeyValue{}
+			file, err := os.Open(filename)
+			if err != nil {
+				log.Fatalf("cannot open %v", filename)
+			}
+			content, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Fatalf("cannot read %v", filename)
+			}
+			file.Close()
+			kva := mapFunction(filename, string(content))
+			intermediate = append(intermediate, kva...)
+			sort.Sort(ByKey(intermediate))
+			intermediateFileName := "mr-" + strconv.Itoa(replyMapTask.MapTaskID) + "-" + strconv.Itoa(replyMapTask.ReduceTaskID)
+			inFile, err1 := os.Create(intermediateFileName)
+			if err1 != nil {
+				log.Fatalf("cannot create %v", filename)
+			}
+			writeToJSON(inFile, intermediate)
+			fileState, _ := os.Stat("intermediateFileName")
+			args1 := Args{false,"", replyMapTask.MapTaskID, -1,
+				intermediateFileName, fileState.Size()}
+			replyMapCompleted := RPCCall(&args1)
+			if replyMapCompleted.RPCState == true {
+				continue
+			}
+		}
+	}()
+	go func() {
+		args := new (Args)
+		args.ApplyMap = false
+		for {
+			replyReduceTask := RPCCall(args)
+			if replyReduceTask.ReduceTaskID == -1 {
+				break
+			}
+			file,_ := os.Open(replyReduceTask.IntermediateFileName)
+			intermediate := readFromJSON(file)
+			i := 0
+			for i < len(intermediate) {
+				j := i + 1
+				for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+					j++
+				}
+				values := []string{}
+				for k := i; k < j; k++ {
+					values = append(values, intermediate[k].Value)
+				}
+				output := reduceFunction(intermediate[i].Key, values)
+				// this is the correct format for each line of Reduce output.
+				fmt.Fprintf(file, "%v %v\n", intermediate[i].Key, output)
+				i = j
+			}
+			file.Close()
+			replyReduceCompleted := RPCCall(args)
+			if replyReduceCompleted.RPCState == true {
+				continue
+			}
+		}
+	}()
 }
 
-// Call 'function' show how to make an RPC call to the coordinator.
-func Call() *Reply {
-	args := Args{}
+// RPCCall :make an RPC call to the coordinator.
+func RPCCall(args *Args) *Reply {
 	reply := Reply{}
 	// send the RPC request, wait for the reply.
-	call("Coordinator.ApplyMapTask", &args, &reply)
+	if args.ApplyMap == true {
+		call("Coordinator.ApplyMapTask", &args, &reply)
+	} else if args.MapCompletedID != -1{
+		call("Coordinator.CorrectMapTaskState", &args, &reply)
+	} else if args.ReduceCompletedID != -1 {
+		call("Coordinator.CorrectReduceTaskState", &args, &reply)
+	} else {
+		call("Coordinator.ApplyReduceTask", &args, &reply)
+	}
 	return &reply
 	//fmt.Printf("reply.Y %v\n", reply.MapFileName)
 }
@@ -90,4 +152,27 @@ func call(rpcName string, args interface{}, reply interface{}) bool {
 	}
 	fmt.Println(err)
 	return false
+}
+
+func writeToJSON(file *os.File,data []KeyValue) {
+	encoder := json.NewEncoder(file)
+	for _,kv := range data {
+		err := encoder.Encode(&kv)
+		if err != nil {
+			log.Fatal("encode:", err)
+		}
+	}
+}
+
+func readFromJSON(file *os.File) []KeyValue {
+	decoder := json.NewDecoder(file)
+	kva := []KeyValue{}
+	for {
+		var kv KeyValue
+		if err := decoder.Decode(&kv); err != nil {
+			break
+		}
+		kva = append(kva,kv)
+	}
+	return kva
 }
